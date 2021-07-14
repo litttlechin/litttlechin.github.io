ebe7bfc46467356dec635dd20b3efe8e79987053
littlechin <littlechintw@gmail.com>
2021-07-14 15:29:22 +0800

    Add ga

--- src/main.js
+++ src/main.js
@@ -3,8 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
 
 Vue.config.productionTip = false
+
+import VueGtag from "vue-gtag";
+
+const vue_router = new VueRouter({
+  routes: [
+    { name: 'Home', path: '/' },
+  ]
+});
+
+Vue.use(VueGtag, {
+  config: { id: "UA-128753148-1" }
+}, vue_router);
 
 new Vue({
   router,
